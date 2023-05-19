1d2227ed49e52be3b005cf00a56dc5b03c0b8bec
Farhan Tahmid <farhantahmid881@gmail.com>
2023-05-19 11:20:25 +0600

    job scheduling with dynamic programming

--- job scheduling dynamic programming.py
+++ job scheduling dynamic programming.py
@@ -1,35 +1,92 @@
-def job_scheduling_dp(jobs):
-    # Compute the number of jobs and the maximum deadline
-    n = len(jobs)
-    d = max(jobs, key=lambda x: x[1])[1]
+import random
+import time
+import matplotlib.pyplot as plt
 
-    # Initialize the dp array with 0 values
-    dp = [[0 for _ in range(d+1)] for _ in range(n+1)]
+def generateRandomJobs(n):
+    
+    jobs = []
 
-    # Compute the maximum profit for each job and deadline using dynamic programming
-    for i in range(1, n+1):
-        for t in range(1, d+1):
-            job = jobs[i-1]
-            if job[1] <= t:
-                dp[i][t] = max(dp[i-1][t], dp[i-1][t-job[1]] + job[2])
-            else:
-                dp[i][t] = dp[i-1][t]
+    for i in range(n):
+        job_id = random.randint(1,1000)
+        deadline = random.randint(1, 2000)
+        profit = random.randint(0,500)
+        job_tuple = (job_id, deadline, profit)
+        jobs.append(job_tuple)
 
-    # Find the maximum profit and the sequence of jobs with their names, profits, and deadlines
-    max_profit = dp[n][d]
-    job_sequence = []
-    t = d
-    for i in range(n, 0, -1):
-        if dp[i][t] != dp[i-1][t]:
-            name, deadline, profit = jobs[i-1]
-            job_sequence.append((name, profit, deadline))
-            t -= deadline
-    job_sequence.reverse()
+    return jobs
 
-    # Return the maximum profit and the sequence of jobs
-    return max_profit, job_sequence
 
-jobs = [('job1', 4, 20), ('job2', 1, 10), ('job3', 1, 40), ('job4', 1, 30)]
-total_profit, job_sequence = job_scheduling_dp(jobs)
-print(total_profit)  # 60
-print(job_sequence)  # ['job3', 'job1', 'job4']
+def getJobProfit(job):
+    return job[2]
+
+def getJobDeadline(job):
+    return job[1]
+
+def jobSchedulingWithDynamicProgramming(jobs):
+    # Sort jobs in descending order based on profits
+    jobs.sort(key=getJobProfit, reverse=True)
+
+    
+    # getting the maximum deadline
+    maxDeadline = max(jobs, key=getJobDeadline)[1]  
+    dynamic = [0] * (maxDeadline+1)
+    #createing job sequence with max deadline
+    jobSequence = [''] * (maxDeadline+1)
+
+    # Traverse through jobs
+    for job in jobs:
+        deadline = job[1]
+        profit = job[2]
+
+        # Find the earliest available slot for the current job
+        while deadline > 0 and dynamic[deadline] != 0:
+            deadline -= 1
+
+        # Update dp and job_sequence lists
+        if deadline > 0:
+            dynamic[deadline] = profit
+            jobSequence[deadline] = job[0]
+
+    # Find the maximum profit and corresponding job sequence
+    maxProfit = sum(dynamic)
+    maxJobSequence = []
+    for job in jobSequence:
+        if job != '':
+            maxJobSequence.append(job)
+
+    return maxProfit, maxJobSequence
+
+def resultOfAlgorithm(numberOfJobs,timeRequired):
+    #plotting the graph
+    plt.plot(numberOfJobs,timeRequired)
+    
+    plt.xlabel("Number Of Jobs")
+    plt.ylabel("Time Required")
+    plt.title("Job Scheduling with Brute Force Algorithm")
+    plt.show()
+
+print("Note: Enter 0 to end the program and generate Results")
+
+#initializing matrix for number of jobs and time required
+listNumber0fJobs=[]
+timeRequired=[]
+
+while True:
+    #Enter number of jobs you want to generate
+    numberOfJobs=int(input("Enter the number of jobs you want to generate: "))
+    if(numberOfJobs==0):
+        resultOfAlgorithm(numberOfJobs=listNumber0fJobs,timeRequired=timeRequired)
+        break
+    print(numberOfJobs)
+    listNumber0fJobs.append(numberOfJobs)
+
+    #Generate random jobs using functions
+    jobs=generateRandomJobs(numberOfJobs)
+    
+    startOfExecution=time.time()
+    #implementing brute force Algorithm
+    maxProfit,jobSequence=jobSchedulingWithDynamicProgramming(jobs=jobs)
+    print(f"Max profit will be: {maxProfit}")
+    print(f"Job sequence of Execution will be: {jobSequence}")
+    endOfExecution=time.time()
+    timeRequired.append(round(endOfExecution-startOfExecution,4))
